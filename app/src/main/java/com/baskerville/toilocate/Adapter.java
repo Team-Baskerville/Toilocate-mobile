68bb533992a3ca4671703056e3cbbdffce66a93f
ChameeraRupasinghe <chameera.15@cse.mrt.ac.lk>
2020-01-11 01:31:15 +0530

    Cardview UI improved

--- app/src/main/java/com/baskerville/toilocate/Adapter.java
+++ app/src/main/java/com/baskerville/toilocate/Adapter.java
@@ -1,9 +1,11 @@
 package com.baskerville.toilocate;
 
 import android.content.Context;
+import android.content.Intent;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.RatingBar;
 import android.widget.TextView;
 
 import androidx.annotation.NonNull;
@@ -11,6 +13,7 @@ import androidx.recyclerview.widget.RecyclerView;
 
 import com.baskerville.toilocate.classes.Config;
 import com.baskerville.toilocate.classes.Toilet;
+import com.baskerville.toilocate.dto.ToiletLiteDTO;
 import com.bumptech.glide.Glide;
 import com.bumptech.glide.request.RequestOptions;
 
@@ -22,33 +25,34 @@ public class Adapter extends RecyclerView.Adapter<Adapter.ViewHolder> {
 
     private LayoutInflater layoutInflater;
     private List<Toilet> toilets;
+    private Toilet currentToilet;
+    private Context context;
 
-    public Adapter(Context context, List<Toilet> toilets){
+    public Adapter(Context context, List<Toilet> toilets) {
         this.layoutInflater = LayoutInflater.from(context);
         this.toilets = toilets;
+        this.context = context;
     }
 
     @NonNull
     @Override
     public ViewHolder onCreateViewHolder(@NonNull ViewGroup viewGroup, int viewType) {
-        View view =  layoutInflater.inflate(R.layout.card_view, viewGroup, false);
+        View view = layoutInflater.inflate(R.layout.card_view, viewGroup, false);
         return new ViewHolder(view);
     }
 
     @Override
     public void onBindViewHolder(@NonNull ViewHolder viewHolder, int position) {
-
-        String locationString = toilets.get(position).getLocation().latitude
-                + ", "+ toilets.get(position).getLocation().longitude;
+        currentToilet = toilets.get(position);
 
         viewHolder.title.setText(toilets.get(position).getName());
-        viewHolder.gender.setText(toilets.get(position).getGender());
-        viewHolder.rating.setText(String.valueOf(toilets.get(position).getRating()));
-        viewHolder.location.setText(locationString);
-        Glide.with(layoutInflater.getContext()).load(Config.BASE_URL+toilets.get(position)
+        viewHolder.gender.setText(String.format("Gender :%s", toilets.get(position).getGender()));
+        viewHolder.ratingBar.setRating(toilets.get(position).getRating());
+        Glide.with(layoutInflater.getContext()).load(Config.BASE_URL + toilets.get(position)
                 .getImagePath())
                 .apply(new RequestOptions().override(200, 300))
                 .into(viewHolder.toiletImage);
+
     }
 
     @Override
@@ -56,18 +60,25 @@ public class Adapter extends RecyclerView.Adapter<Adapter.ViewHolder> {
         return toilets.size();
     }
 
-    public class ViewHolder extends RecyclerView.ViewHolder{
+    public class ViewHolder extends RecyclerView.ViewHolder {
 
-        TextView title, gender, rating, location;
+        TextView title, gender, rating;
         CircleImageView toiletImage;
+        RatingBar ratingBar;
 
         public ViewHolder(@NonNull View itemView) {
             super(itemView);
             title = itemView.findViewById(R.id.textViewCardTitle);
             gender = itemView.findViewById(R.id.textViewCardGender);
-            rating = itemView.findViewById(R.id.textViewCardRating);
-            location = itemView.findViewById(R.id.textViewCardLocation);
+            ratingBar = itemView.findViewById(R.id.ratingBarCard);
             toiletImage = itemView.findViewById(R.id.imageNearbyToilet);
+
+            itemView.setOnClickListener(v ->
+            {
+                Intent toiletDetailsIntent = new Intent(itemView.getContext(), ToiletDetails.class);
+                toiletDetailsIntent.putExtra("toilet", new ToiletLiteDTO(currentToilet));
+                context.startActivity(toiletDetailsIntent);
+            });
 
         }
     }
